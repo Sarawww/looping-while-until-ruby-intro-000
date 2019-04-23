aa215f06ddb7bde55a2c4ffd6b18b0078044209d
concurrent-input-6175 <xywong23@gmail.com>
2019-04-23 07:37:31 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -2,7 +2,7 @@ def using_while
   levitation_force = 6
   
   while levitation_force < 10 
-    puts "Your levitation force is #{levitation_force}"
+    puts ""
     levitation_force += 1 
   end
 end
